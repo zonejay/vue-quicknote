af58bc68f4e85c103c4e47c1f63267a699707fdc
zonejay <zyj1179035793@gmail.com>
2024-07-11 21:27:38 +0800

    Create demo.vue

---
+++ components/demo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="parent">
+    <ImageScrollList v-model="selectedImage" :images="images" />
+    <select v-model="selectedImage">
+      <option v-for="image in images" >{{image}}</option>
+    </select>
+    {{selectedImage}}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import ImageScrollList from './ImageScrollList.vue';
+
+const images = [
+  'image1.jpg',
+  'image2.jpg',
+  'image3.jpg',
+  'image4.jpg',
+  'image5.jpg',
+  'image6.jpg'
+  // 更多图片...
+];
+
+const selectedImage = ref('image6.jpg');
+</script>
+<style>
+.parent {
+  width: 240px;
+}
+</style>
